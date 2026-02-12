3c3b612856db175b35c88866ee4717e3aba36551
nataliaradcuk <nataliia.radchuk@oa.edu.ua>
2026-02-12 15:19:49 +0200

    Add 4 UI pages: containers, products, create container, create product

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,35 +1,50 @@
-import './Layout.css'
+import "./Layout.css";
 
 export default function Layout() {
-
   return (
     <div className="layout">
-        <div className="side-panel">
-            <div className="logo-div">
-                <img className="logo" src="./public/Logo.svg" alt="Logo" />
-            </div>
-            <div className="nav-links">
-                <div className="nav-item">
-                    <img className="nav-img qr"  src="./public/qr.svg" alt="qr" />
-                    <a className="nav-link" href="#">Scan QR</a>
-                </div>
-                <div className="nav-item">
-                    <img className="nav-img box" src="./public/box.svg" alt="box" />
-                    <a className="nav-link" href="#">Container</a>
-                </div>
-                <div className="nav-item">
-                    <img className="nav-img shopping-basket"  src="./public/shopping-basket.svg" alt="shopping basket" />
-                    <a className="nav-link" href="#">Products</a>
-                </div>
-                <div className="nav-item">
-                    <img className="nav-img users-alt"  src="./public/users-alt.svg" alt="users" />
-                    <a className="nav-link" href="#">Users</a>
-                </div>
-            </div>
+      <div className="side-panel">
+        <div className="logo-div">
+          <img className="logo" src="./public/Logo.svg" alt="Logo" />
         </div>
-        <div className="top-panel">
-            <a href="">dsc</a>
+        <div className="nav-links">
+          <div className="nav-item">
+            <img className="nav-img qr" src="./public/qr.svg" alt="qr" />
+            <a className="nav-link" href="#">
+              Scan QR
+            </a>
+          </div>
+          <div className="nav-item">
+            <img className="nav-img box" src="./public/box.svg" alt="box" />
+            <a className="nav-link" href="#">
+              Container
+            </a>
+          </div>
+          <div className="nav-item">
+            <img
+              className="nav-img shopping-basket"
+              src="./public/shopping-basket.svg"
+              alt="shopping basket"
+            />
+            <a className="nav-link" href="#">
+              Products
+            </a>
+          </div>
+          <div className="nav-item">
+            <img
+              className="nav-img users-alt"
+              src="./public/users-alt.svg"
+              alt="users"
+            />
+            <a className="nav-link" href="#">
+              Users
+            </a>
+          </div>
         </div>
+      </div>
+      <div className="top-panel">
+        <a href="">dsc</a>
+      </div>
     </div>
   );
 }
