3f7cd95dbe55be73800d8e6972b8863b01aa1efc
Unding <samanta.verdoes@gmail.com>
2026-02-13 15:59:47 +0200

    Layout added for existing pages

--- src/components/AppRoute.jsx
+++ src/components/AppRoute.jsx
@@ -1,15 +1,20 @@
-import {Route, Routes} from "react-router-dom";
-import Layout from "./Layout"
-import Home from "../pages/Home"
-import TodoPage from "../pages/TodoPage";
+import { Route, Routes } from "react-router-dom";
+import Layout from "./layout/Layout";
+import ProductsPage from "../pages/ProductsPage/ProductsPage";
+import ContainersPage from "../pages/ContainersPage/ContainersPage";
+import CreateProductPage from "../pages/CreateProductPage/CreateProductPage";
+import CreateContainerPage from "../pages/CreateContainerPage/CreateContainerPage";
 
-export default function AppRoute(){
-    return(
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="containers" element={<ContainersPage />} />
-          </Route>
-        </Routes>
-    )
-};
+export default function AppRoute() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<ProductsPage />} />
+        <Route path="products" element={<ProductsPage />} />
+        <Route path="containers" element={<ContainersPage />} />
+        <Route path="products/new" element={<CreateProductPage />} />
+        <Route path="containers/new" element={<CreateContainerPage />} />
+      </Route>
+    </Routes>
+  );
+}
